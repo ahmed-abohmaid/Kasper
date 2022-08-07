58b86ecb2f3ccfbc8a56560f64da0dbc912df2d2
Ahmed-AboHmaiD <109979191+Ahmed-AboHmaiD@users.noreply.github.com>
2022-08-07 19:04:17 +0200

    JS Upload

---
+++ kasper.js
@@ -0,0 +1,26 @@
+// skills-sprogress
+let section = document.querySelector(".our-skills");
+let progressSpans = document.querySelectorAll(".prog-holder .prog span");
+// Up Bottun
+let span = document.querySelector(".up");
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop - 260) {
+    progressSpans.forEach((span) => {
+      span.style.width = span.dataset.progress;
+    });
+  }
+  // Up Bottun
+  if (this.scrollY >= 1000) {
+    span.classList.add("show");
+  } else {
+    span.classList.remove("show");
+  }
+};
+
+// Up Bottun
+span.onclick = function () {
+  window.scrollTo({
+    top: 0,
+  });
+};
